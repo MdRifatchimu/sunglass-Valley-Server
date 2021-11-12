73250cff7ca81ed4e0ec8ff1d2b137d1eb865933
MdRifatchimu <himu2411@swfu.edu.cn>
2021-11-13 03:09:15 +0600

    second part added

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function run() {
       res.json(products);
     });
 
-    // get single product
+    // get single product.
     app.get("/products/:id", async (req, res) => {
       const id = req.params.id;
       const query = {_id: ObjectId(id)};
